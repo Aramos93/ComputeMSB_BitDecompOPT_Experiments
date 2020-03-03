e66655d431d3e908fa3d6241584394a01573521b
Alexander Ramos <aara@itu.dk>
2020-03-03 17:43:35 +0100

    fixed some shareconvert bugs, and started on MSB

--- ComputeMSB.py
+++ ComputeMSB.py
@@ -9,6 +9,7 @@ import time
 ##################################################### Globals ########################################################
 L = 4
 p = 11
+seed = random.randint(0,100)
 file1 = "shares0.txt"
 file2 = "shares1.txt"
 
@@ -33,7 +34,7 @@ class MyType:
 
 ######################################################################################################################
 
-########################################### Party (Create p0, p1, p2) ##############################################
+########################################### Party (Create p0, p1, p2) Protocols are here #############################
 
 class Party:
     #### Initialize Party ######
@@ -52,6 +53,7 @@ class Party:
         self.party = "p"+str(partyName)
         self.partyName = partyName
         self.shares = []
+        self.converted_shares = []
         if partyName == 0:
             self.readFile(file1)
         elif partyName == 1:
@@ -156,35 +158,37 @@ class Party:
         return MyType(r, is_zl=in_zl), MyType(x-r, is_zl=in_zl)
     
     # Mimic private compare - actually just compares reconstructed value
-    def dummyPC(self, x, r, beta):    
+    def dummyPC(self, x, r, beta):   
         return beta.x ^ (x.x > r.x) 
         
     # Convert a shares of some value a in ZL to shares of the same value in ZL-1
     def shareConvert(self, a=MyType(0)):
         if self.party == "p0" or self.party == "p1":
             rec = self.reconstruct2PCSingleInt(a) # This reconstruction is only to check value of a
-        
-        #replace these with actual values
-        n_prime_prime = MyType(1)
-        r = MyType(7)
-        r_0 = MyType(3)
-        r_1 = MyType(4)
-        alpha = self.wrap(r_0, r_1)
 
+        # Use same random seed to ensure all parties share same common randomness: r, r_0, r_1, n''
+        random.seed(seed)
+        n_prime_prime = MyType(random.randint(0,1))
+        r = MyType(random.randint(1,2**L)) #can't be 0 for some reason. Probably handled in PC which we don't implement
+
+        r_0, r_1 = self.generateMyTypeShares(r.x)
+        alpha = self.wrap(r_0, r_1)
+        
         if(self.party == "p0"):
-            if(rec.x == (2**L)-1):
-                raise Exception(f"Reconstructed value 'a' is {rec.x} and L-1 is {(2**L)-1} which is not allowed according to protocol")
+            if rec.x == ((2**L) - 1) :
+                raise Exception(f"Reconstructed value 'a' is {rec.x} == L-1 {(2**L)-1} which is not allowed according to protocol")
             a_tilde = a + r_0
             beta = self.wrap(a, r_0)
             self.sendInt("p2", a_tilde.x)
             x_bit_0 = self.recvShares("p0") # bit shares of x not used because PC not implemented (only dummied)
             delta_0 = self.recvInt("p0")
             n_prime_0 = self.recvInt("p0")
-            n_0 = MyType(n_prime_0 + (1 - self.partyName)*n_prime_prime.x - 2*n_prime_prime.x * n_prime_0, is_zl=False)
+            n_0 = MyType(n_prime_0 + (1 - self.partyName)*n_prime_prime.x - 2 * n_prime_prime.x * n_prime_0, is_zl=False)
             theta = MyType(beta + (1 - self.partyName) * (-alpha - 1) + delta_0 + n_0.x, is_zl=False)
             y_0 = MyType(a.x-theta.x, is_zl=False)
 
-            print("After convert - p0 a:",y_0.x)
+            #print("After convert - p0 a:",y_0.x)
+            self.converted_shares.append(y_0)
             return y_0
         if(self.party == "p1"):
             a_tilde = a + r_1
@@ -197,13 +201,13 @@ class Party:
             theta = MyType(beta + (1 - self.partyName) * (-alpha - 1) + delta_1 + n_1.x, is_zl=False)
             y_1 = MyType(a.x-theta.x, is_zl=False)
 
-            print("After convert - p1 a:", y_1.x)
+            #print("After convert - p1 a:", y_1.x)
+            self.converted_shares.append(y_1)
             return y_1
             
         if(self.party == "p2"):
             a_tilde_0 = MyType(self.recvInt("p2"))
             a_tilde_1 = MyType(self.recvInt("p2"))
-
             x = a_tilde_0 + a_tilde_1
             delta = self.wrap(a_tilde_0, a_tilde_1)
 
@@ -221,18 +225,41 @@ class Party:
            
 
             self.sendInt("p0", n_prime_0.x); self.sendInt("p1", n_prime_1.x)
-            
+    
 
-        
-     
+    def computeMSB(self, a=MyType(0)):
+        if self.party == "p0" or self.party == "p1":
+            rec = self.reconstruct2PCSingleInt(a)
+        random.seed(seed)
+        beta = MyType(random.randint(0,1),is_zl=False)
 
-        
+        if self.party == "p0":
+            if rec.x >= ((2**L) - 1):
+                raise Exception(f"Reconstructed value 'a' is {rec.x} which is NOT in ZL-1 {(2**L) -1}")
+            #x_0 = self.recvInt("p0")
+            #x_bit_arr_0 = self.recvShares("p0")
+            #x_firstBit_0 = self.recvInt("p0")
+        #if self.party == "p1":
+            #x_1 = self.recvInt("p1")
+            #x_bit_arr_1 = self.recvShares("p1")
+            #x_firstBit_1 = self.recvInt("p1")
+        if self.party == "p2":
+            x = MyType(random.randint(0, (2**L) - 1), is_zl=False)
+            print(x.x)
+
+
+
+  
 ######################################################################################################################    
 
 
 
-
 ####################################################### Tests ########################################################
+
+parties = []
+p0 = Party(0); p1 = Party(1); p2 = Party(2)
+parties.append(p0); parties.append(p1)
+
 def test_MyType():
     testListInput = [8,20,16,0,-3]
     testListRes = []
@@ -250,18 +277,12 @@ def test_Party():
     print("p2 shares (empty): ", p2.shares)
 
 def test_reconstruct2PC():
-    parties = []
-    p0 = Party(0); p1 = Party(1)
-    parties.append(p0); parties.append(p1)
     print(p0.getShareVals()); print(p1.getShareVals())
     for p in parties:
         thread = threading.Thread(target=p.reconstruct2PC,args=())
         thread.start()
 
 def test_shareConvert():
-    parties = []
-    p0 = Party(0); p1 = Party(1); p2 = Party(2)
-    parties.append(p0); parties.append(p1)
     for c in range(len(p0.shares)):
 
         thread = threading.Thread(target=p2.shareConvert, args=())
@@ -270,23 +291,52 @@ def test_shareConvert():
 
         threads = [None]*len(parties)
         for i, p in enumerate(parties):
-            print(f"Before convert - {p.party} a: {p.shares[c].x}")
+            #print(f"Before convert - {p.party} a: {p.shares[c].x}")
             threads[i] = threading.Thread(target=p.shareConvert, args=(p.shares[c],))
             threads[i].start()
             time.sleep(0.1)
         
-        for t in threads:
+        for p,t in zip(parties, threads):
             t.join(2)
         thread.join(2)
-        print("")
+        #print("")
+    print("Shares in ZL")    
+    print([s.x for s in p0.shares])
+    print([s.x for s in p1.shares])
+    print("Reconstructed: ", [(s0+s1).x for s0,s1 in zip(p0.shares,p1.shares)])
+    print()
+    print("Shares in ZL-1")
+    print([s.x for s in p0.converted_shares])
+    print([s.x for s in p1.converted_shares])
+    print("Reconstructed: ", [MyType(s0.x+s1.x, is_zl=False).x for s0,s1 in zip(p0.converted_shares,p1.converted_shares)])
 
 
+def test_computeMSB():
+    
+    p0.converted_shares = [MyType(10),MyType(1),MyType(3),MyType(7),MyType(10),MyType(2),MyType(3),MyType(7),MyType(5),MyType(11)]
+    p1.converted_shares = [MyType(2),MyType(7),MyType(11),MyType(14),MyType(5),MyType(12),MyType(12),MyType(0),MyType(10),MyType(7)]
+        
+    #for c in range(len(p0.shares)):
 
+       
 
-def test_ConvertTo3PShares():
-    print("implement this")
+    threads = [None]*len(parties)
+    for i, p in enumerate(parties):
+        #print(f"Before convert - {p.party} a: {p.shares[c].x}")
+        threads[i] = threading.Thread(target=p.computeMSB, args=(p.converted_shares[0],))
+        threads[i].start()
+        time.sleep(0.1)
+    
+    thread = threading.Thread(target=p2.computeMSB, args=())
+    thread.start()
+
+    for t in threads:
+        t.join(2)
+    thread.join(2)
+    print("")
 
 #test_shareConvert()
+test_computeMSB()
 #test_Party()    
 #test_reconstruct2PC()
 #test_MyType()
