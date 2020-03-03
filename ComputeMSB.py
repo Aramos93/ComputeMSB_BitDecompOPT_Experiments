a1993b3f812f27b5e48b982520bfd5a603808e4a
Alexander Ramos <aara@itu.dk>
2020-03-03 13:22:01 +0100

    Tests for shareConvert, single value reconstruct, proper check of reconstructed value a in shareconvert, bit better thread crash handling

--- ComputeMSB.py
+++ ComputeMSB.py
@@ -50,6 +50,7 @@ class Party:
         if partyName > 2 or partyName < 0:
             raise Exception("PartyName must be 0, 1, or 2")
         self.party = "p"+str(partyName)
+        self.partyName = partyName
         self.shares = []
         if partyName == 0:
             self.readFile(file1)
@@ -67,14 +68,13 @@ class Party:
             self.shares.append(MyType(int(line.strip())))
 
     
-    # Send own shares to some party (sendTo)
+    ################ Send and Receive shares ###################
     def sendShares(self, sendTo, value):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             pickled = pickle.dumps(value)
             s.connect((self.address, self.lookup.get(sendTo)))
             s.send(pickled)
 
-    # Receive shares from some party
     def recvShares(self,recvParty):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.address, self.lookup.get(recvParty)))
@@ -84,9 +84,23 @@ class Party:
             data_arr = pickle.loads(data)
             return repr(data_arr)
     
-    # Add shares and convert result to be of MyType (number in ZL) 
-    def addMyType(self, x, y):
-        return MyType(x+y).x   
+    
+    ############ Send and Receive single int ####################
+    def sendInt(self,sendTo,value):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.address, self.lookup.get(sendTo)))
+            s.send(bytes(str(value), 'utf8'))
+
+    def recvInt(self,recvParty):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.address, self.lookup.get(recvParty)))
+            s.listen()
+            conn, addr = s.accept()
+            data = conn.recv(1024)
+            strings = str(data, 'utf8')
+            return(int(strings))
+
+      
     
     # Reconstruct secret by sending shares from p1 to p0 and adding them and printing them
     def reconstruct2PC(self):
@@ -94,20 +108,33 @@ class Party:
             self.sendShares("p0",self.getShareVals())
         if self.party == "p0":
             p1_shares = literal_eval(self.recvShares(self.party))
-            reconstructed = [self.addMyType(x,y) for x,y in zip(self.getShareVals(), p1_shares)]
+            reconstructed = [MyType(x+y).x for x,y in zip(self.getShareVals(), p1_shares)]
             print("Reconstructed: ", reconstructed)
-
-
+            return reconstructed
+    
+    # Reconstruct a single secret by sending its share from p1 to p0, adding each share and return result
+    def reconstruct2PCSingleInt(self, a):
+        res =  MyType(0)
+        if self.party == "p1":
+            self.sendInt("p0", a.x)
+            time.sleep(0.1)
+        if(self.party == "p0"):
+            a_1 = self.recvInt(self.party)
+            res = a + MyType(a_1)
+        return res        
+            
+    #Check if a+b > ZL aka wraps around ZL
     def wrap(self, a, b):
         if(a.x + b.x >= 2**L):
             return 1
         else:
             return 0
     
+    # convert integer to L sized bitstring with trailing zeros
     def convertToBitString(self, x):
         return format(x.x, "0"+str(L)+'b')
     
-
+    # Generate shares of each bit of a number x
     def generateBitShares(self, x):
         share0 = []
         share1 = []
@@ -119,6 +146,7 @@ class Party:
 
         return share0, share1
     
+    # Generate shares in either ZL or Zl-1 of a number
     def generateMyTypeShares(self, x, in_zl=True):
         if in_zl == True:
             r = random.randint(0, 2**L)
@@ -127,14 +155,16 @@ class Party:
 
         return MyType(r, is_zl=in_zl), MyType(x-r, is_zl=in_zl)
     
+    # Mimic private compare - actually just compares reconstructed value
     def dummyPC(self, x, r, beta):    
         return beta.x ^ (x.x > r.x) 
         
-
+    # Convert a shares of some value a in ZL to shares of the same value in ZL-1
     def shareConvert(self, a=MyType(0)):
+        if self.party == "p0" or self.party == "p1":
+            rec = self.reconstruct2PCSingleInt(a) # This reconstruction is only to check value of a
+        
         #replace these with actual values
-        if(a == 2**L - 1):
-            raise Exception("a can't be ZL-1")
         n_prime_prime = MyType(1)
         r = MyType(7)
         r_0 = MyType(3)
@@ -142,17 +172,34 @@ class Party:
         alpha = self.wrap(r_0, r_1)
 
         if(self.party == "p0"):
+            if(rec.x == (2**L)-1):
+                raise Exception(f"Reconstructed value 'a' is {rec.x} and L-1 is {(2**L)-1} which is not allowed according to protocol")
             a_tilde = a + r_0
             beta = self.wrap(a, r_0)
             self.sendInt("p2", a_tilde.x)
-            x_bit_0 = self.recvShares("p0")
+            x_bit_0 = self.recvShares("p0") # bit shares of x not used because PC not implemented (only dummied)
             delta_0 = self.recvInt("p0")
+            n_prime_0 = self.recvInt("p0")
+            n_0 = MyType(n_prime_0 + (1 - self.partyName)*n_prime_prime.x - 2*n_prime_prime.x * n_prime_0, is_zl=False)
+            theta = MyType(beta + (1 - self.partyName) * (-alpha - 1) + delta_0 + n_0.x, is_zl=False)
+            y_0 = MyType(a.x-theta.x, is_zl=False)
+
+            print("After convert - p0 a:",y_0.x)
+            return y_0
         if(self.party == "p1"):
             a_tilde = a + r_1
             beta = self.wrap(a, r_1)
             self.sendInt("p2", a_tilde.x)
-            x_bit_1 = self.recvShares("p1")
+            x_bit_1 = self.recvShares("p1") # bit shares of x not used because PC not implemented (only dummied)
             delta_1 = self.recvInt("p1")
+            n_prime_1 = self.recvInt("p1")
+            n_1 = MyType(n_prime_1 + (1 - self.partyName) * n_prime_prime.x - 2 * n_prime_prime.x * n_prime_1, is_zl=False)
+            theta = MyType(beta + (1 - self.partyName) * (-alpha - 1) + delta_1 + n_1.x, is_zl=False)
+            y_1 = MyType(a.x-theta.x, is_zl=False)
+
+            print("After convert - p1 a:", y_1.x)
+            return y_1
+            
         if(self.party == "p2"):
             a_tilde_0 = MyType(self.recvInt("p2"))
             a_tilde_1 = MyType(self.recvInt("p2"))
@@ -171,41 +218,14 @@ class Party:
 
             n_prime = self.dummyPC(x, r-MyType(1), n_prime_prime)
             n_prime_0, n_prime_1 = self.generateMyTypeShares(n_prime,False)
+           
 
-            self.sendInt("p0", n_prime_0); self.sendInt("p1", n_prime_1)
+            self.sendInt("p0", n_prime_0.x); self.sendInt("p1", n_prime_1.x)
             
 
         
      
 
-
-
-
-    
-
-    ############ Send and Receive for single int ####################
-    def sendInt(self,sendTo,value):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.address, self.lookup.get(sendTo)))
-            s.send(bytes(str(value), 'utf8'))
-
-    def recvInt(self,recvParty):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.address, self.lookup.get(recvParty)))
-            s.listen()
-            conn, addr = s.accept()
-            data = conn.recv(1024)
-            strings = str(data, 'utf8')
-            return(int(strings))
-
-
-
-        
-
-
-    
-    # def convertToZlMinusOne(self, toZL = False ):
-    #     self.shares = [MyType(e, toZL).x for e in self.shares]
         
 ######################################################################################################################    
 
@@ -218,9 +238,7 @@ def test_MyType():
     testListRes = []
     for e in testListInput:
         testListRes.append(MyType(e).x)
-
-    for x in testListRes:
-        print(x)
+    print(testListRes)
 
 
 def test_Party():
@@ -244,21 +262,31 @@ def test_shareConvert():
     parties = []
     p0 = Party(0); p1 = Party(1); p2 = Party(2)
     parties.append(p0); parties.append(p1)
-    thread = threading.Thread(target=p2.shareConvert, args=())
-    thread.start()
-    time.sleep(0.1)
-    for p in parties:
-        thread = threading.Thread(target=p.shareConvert, args=(p.shares[0],))
+    for c in range(len(p0.shares)):
+
+        thread = threading.Thread(target=p2.shareConvert, args=())
         thread.start()
         time.sleep(0.1)
-    
+
+        threads = [None]*len(parties)
+        for i, p in enumerate(parties):
+            print(f"Before convert - {p.party} a: {p.shares[c].x}")
+            threads[i] = threading.Thread(target=p.shareConvert, args=(p.shares[c],))
+            threads[i].start()
+            time.sleep(0.1)
+        
+        for t in threads:
+            t.join(2)
+        thread.join(2)
+        print("")
+
 
 
 
 def test_ConvertTo3PShares():
     print("implement this")
 
-test_shareConvert()
+#test_shareConvert()
 #test_Party()    
 #test_reconstruct2PC()
 #test_MyType()
