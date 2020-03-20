83c7f179808724ca34caf49c31c9296eaceaaca9
Emil H <echh@itu.dk>
2020-03-20 14:49:32 +0100

    more changes

--- TestLogComp.py
+++ TestLogComp.py
@@ -1,5 +1,5 @@
-def multShares(x,y):
-    res = x*y % 2
+def multShares(x0,x1,y0,y1):
+    res = ((x0+x1)%2)*((y0+y1)%2) % 2
     return 0, res
 
 
@@ -18,7 +18,7 @@ alice0 = [0,0,0,0,0,0,0,0]
 bob1 =   [0,0,0,0,0,0,0,0]
 bob0 =   [0,0,1,1,0,0,1,1]    
 
-a,b = multShares(alice1[7],bob0[7])
+a,b = multShares(alice1[7],alice0[7],bob0[7],bob1[7])
 alicec_1_0 = [a]
 alicec_1_1 = [a]
 
@@ -33,7 +33,7 @@ bobx_1_1 = [bob1[7]+bob0[7] % 2]
 
 
 for i in range(6,-1,-1) :  
-    a,b = multShares(alice1[i],bob0[i])
+    a,b = multShares(alice1[i],alice0[i],bob0[i],bob1[i])
     alicec_1_0.insert(0,a)
     bobc_1_0.insert(0,b)
     alicec_1_1.insert(0,(a+alice1[i]+alice0[i])%2)
@@ -43,9 +43,24 @@ for i in range(6,-1,-1) :
     bobx_1_0.insert(0,(bob1[i]+bob0[i])%2)
     bobx_1_1.insert(0,(bob1[i]+bob0[i]+1)%2)
 
+a,b = multShares(alice0[6],alice1[6],bob0[6],bob1[6])
+toalice = a^(alice0[6]*alicec_1_0[7])^(alice1[6]*alicec_1_0[7])
+tobob = b^(bob0[6]*bobc_1_0[7])^(bob1[6]*bobc_1_0[7])
+alicec_2_0 = [[toalice,alicec_1_0[7]]]
+alicec_2_1 = [[toalice,alicec_1_0[7]]]
+bobc_2_0 = [[tobob,bobc_1_0[7]]]
+bobc_2_1 = [[tobob,bobc_1_0[7]]]
 
-alicec_2_0 
-alicec_2_1
+for i in range(4,-1,-2) :
+    a,b = multShares(alice0[i],alice1[i],bob0[i],bob1[i])
 
+    toalice = a^(alice0[i]*alicec_1_0[i+2])^(alice1[i]*alicec_1_0[i+2])
+    tobob = b^(bob0[i]*bobc_1_0[i+2])^(bob1[i]*bobc_1_0[i+2])
+    alicec_2_0.insert(0,[alicec_1_0[i],alicec_1_0[i+1]])
+    bobc_2_0.insert(0,[bobc_1_0[i],bobc_1_0[i+1]])
 
-
+    toalice = a^(alice0[i]*alicec_1_1[i+2])^(alice1[i]*alicec_1_1[i+2])
+    tobob = b^(bob0[i]*bobc_1_1[i+2])^(bob1[i]*bobc_1_1[i+2])
+    alicec_2_1.insert(0,[alicec_1_1[i],alicec_1_1[i+1]])
+    bobc_2_1.insert(0,[bobc_1_1[i],bobc_1_1[i+1]])
+        
