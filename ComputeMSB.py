be16d6b1ed60321331808b023b8d817edef48d3c
emil <echh@itu.dk>
2020-04-22 04:27:45 -0400

    less sleep

--- ComputeMSB.py
+++ ComputeMSB.py
@@ -1318,7 +1318,7 @@ def test_bitDecompOptDist():
     start = time.time()
     for c in range(len(party.shares)):
         party.bitDecompOpt(party.shares[c])
-        time.sleep(0.5)
+        time.sleep(0.1)
     end = time.time()
     
 
